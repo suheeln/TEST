62e17152da031dde7eb52986954d1b00a36c21b7
suheel <suheel577116@gmail.com>
2020-02-29 22:51:11 +0530

    My first commit0

---
+++ FirstJavaRun/src/com/first/RunFirst.java
@@ -0,0 +1,12 @@
+package com.first;
+
+public class RunFirst {
+
+	public static void main(String[] args) {
+ /*
+  * This is first program runing after knowing JAR, JDK, JRE and JVM
+  */
+		System.out.println("Run like never before...never after!!!!");
+	}
+
+}
